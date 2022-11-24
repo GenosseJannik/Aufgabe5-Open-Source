04c985393df0b90103a3a797def02d98f5f911d8
Jannik Seeburger <jannik.seeburger@continental-corporation.com>
2022-11-24 12:46:29 +0100

    Hinzufügen eines Doc Kommentares
    JOOOO

--- src/main/java/de/hfu/App.java
+++ src/main/java/de/hfu/App.java
@@ -9,6 +9,9 @@ public class App
 {
     public static void main( String[] args )
     {
+    	/**
+    	 * Beginn des Programms
+    	 */
         Scanner scan = new Scanner(System.in);
         String eingabe = scan.next();
         System.out.println(eingabe.toUpperCase());
